e4a41a344d6e44b272a02f87dadbe79bd48abef6
administrator <liquidnuker@users.noreply.github.com>
2017-08-01 21:03:29 -0700

    converted to module pattern

--- src/faqListToggle1.js
+++ src/faqListToggle1.js
@@ -1,23 +1,24 @@
 import "./js/vendor/polyfill_nextElementSibling.js";
 
-const faqList1_01 = {
-  init: function (list) {
-    this.addEventListener();
-    this.toggleAllContents(list, "none");
-    list.faqList1_01_EL("click", this.toggleContent);
-  },
-  addEventListener: function () {
+const faqList1_01 = (function () {
+  function init(list) {
+    list = document.querySelectorAll(list);
+    addEvents();
+    toggleAllContents(list, "none");
+    list.faqList1_01_EL("click", toggleContent);
+  }
+
+  function addEvents() {
     NodeList.prototype.faqList1_01_EL = function (event, func) {
       this.forEach(function (content, item) {
         content.addEventListener(event, func);
       });
     };
-  },
-  getElements: function (elmSel) {
-    return document.querySelectorAll(elmSel);
-  },
-  toggleContent: function () {
+  }
+
+  function toggleContent() {
     const x = this.nextElementSibling;
+
     if (x.style.display === "none") {
       this.className += " fq";  
       x.style.display = "block";
@@ -25,12 +26,17 @@ const faqList1_01 = {
       this.className = "faqList1-01_title";  
       x.style.display = "none";
     }
-  },
-  toggleAllContents: function (list, display) {
+  }
+
+  function toggleAllContents(list, display) {
     for (let i of list) {
       i.nextElementSibling.style.display = display;
     }
   }
-};
+
+  return {
+    init: init
+  };
+})();
 
 export {faqList1_01};
--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import "./styles/main.scss";
 
 import {faqList1_01} from "./faqListToggle1.js";
 
-const list = faqList1_01.getElements(".faqList1-01_title");
-faqList1_01.init(list);
+faqList1_01.init(".faqList1-01_title");
